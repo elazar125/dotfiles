c5bd2bf6f339d1dc53425055e73c8b2882647eec
Ezra Lazar <elazar125@gmail.com>
2023-04-30 20:59:05 -0500

    Add gitconfig, hide shopt errors

--- bash/shopt.sh
+++ bash/shopt.sh
@@ -1,22 +1,22 @@
-shopt -s cdspell
-shopt -s checkwinsize
-shopt -s cmdhist
-shopt -s complete_fullquote
-shopt -s dirspell
-shopt -s dotglob
-shopt -s expand_aliases
-shopt -s extglob
-shopt -s extquote
-shopt -s force_fignore
-shopt -s globasciiranges
-shopt -s globskipdots
-shopt -s globstar
-shopt -s histappend
-shopt -s hostcomplete
-shopt -s interactive_comments
-shopt -s nocaseglob
-shopt -s nocasematch
-shopt -s patsub_replacement
-shopt -s progcomp
-shopt -s promptvars
-shopt -s sourcepath
+shopt -s cdspell 2>/dev/null
+shopt -s checkwinsize 2>/dev/null
+shopt -s cmdhist 2>/dev/null
+shopt -s complete_fullquote 2>/dev/null
+shopt -s dirspell 2>/dev/null
+shopt -s dotglob 2>/dev/null
+shopt -s expand_aliases 2>/dev/null
+shopt -s extglob 2>/dev/null
+shopt -s extquote 2>/dev/null
+shopt -s force_fignore 2>/dev/null
+shopt -s globasciiranges 2>/dev/null
+shopt -s globskipdots 2>/dev/null
+shopt -s globstar 2>/dev/null
+shopt -s histappend 2>/dev/null
+shopt -s hostcomplete 2>/dev/null
+shopt -s interactive_comments 2>/dev/null
+shopt -s nocaseglob 2>/dev/null
+shopt -s nocasematch 2>/dev/null
+shopt -s patsub_replacement 2>/dev/null
+shopt -s progcomp 2>/dev/null
+shopt -s promptvars 2>/dev/null
+shopt -s sourcepath 2>/dev/null
