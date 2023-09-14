a49fa5b216c17655b572321324f436a3b09679de
Ezra Lazar <elazar125@gmail.com>
2023-09-14 13:09:43 -0500

    Add useful aliases for Windows development

---
+++ .config/bash/aliases/windows.sh
@@ -0,0 +1,4 @@
+alias hosts='"$EDITOR" /c/Windows/System32/drivers/etc/hosts'
+alias iis='/c/Windows/System32/inetsrv/appcmd.exe'
+alias msb='MSBuild.exe'
+alias msbr='pwsh -c "MSBuild.exe /p:Configuration=Release"'
